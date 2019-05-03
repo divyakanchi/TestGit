7ff6c6926b01ccff791ca5e0159ce243ba8838df
divyakanchi <bindukanchi4@gmail.com>
2019-05-03 18:01:33 -0500

    select.php

---
+++ select.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'connection.php';
+
+$sql = "SELECT id, firstname, lastname FROM MyStudents";
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$con->close();
+?>
